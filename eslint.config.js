17f03d7d9779ebef32d885fd5c05a49462695536
Martins Aloba <aloba.martins@gmail.com>
2026-03-03 22:01:43 -0600

    chore: Migrate ESLint to flat config and add dev tooling

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+const { defineConfig } = require("eslint/config");
+const expoFlat = require("eslint-config-expo/flat");
+
+module.exports = defineConfig([
+  ...expoFlat,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "import/order": [
+        "warn",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          "newlines-between": "always",
+        },
+      ],
+      "no-console": ["warn", { allow: ["error", "warn"] }],
+    },
+  },
+  {
+    ignores: ["node_modules/", ".expo/", "dist/", "supabase/functions/"],
+  },
+]);
